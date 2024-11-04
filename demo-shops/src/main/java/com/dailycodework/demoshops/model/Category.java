f30df252653beea40741152f7a283a39a24aad45
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-11-04 20:59:40 -0300

    projeto eCommerce

---
+++ demo-shops/src/main/java/com/dailycodework/demoshops/model/Category.java
@@ -0,0 +1,11 @@
+package com.dailycodework.demoshops.model;
+import java.util.List;
+
+public class Category {
+
+    private Long id;
+    private String name;
+
+    private List<Product> products;
+
+}
---
+++ demo-shops/src/main/java/com/dailycodework/demoshops/model/Image.java
@@ -0,0 +1,34 @@
+package com.dailycodework.demoshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//oq os construtores realizam
+//oq que cada rraliza no código
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "procuct+id")
+    private Product product;
+
+}
